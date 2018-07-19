b52279adea53bdd962a81bce31af57b5038c3716
Mohamed <mebsim@users.noreply.github.com>
2018-07-19 14:06:36 -0400

    Update loggerjson.php
    Added resources section

--- loggerjson.php
+++ loggerjson.php
@@ -12,6 +12,13 @@ This program has the purpose of converting an MySQL
 database into a text format that a user can access via
 the internet. Formats into a JSON file.
 
+-------------------------------------
+
+Resources Used:
+https://stackoverflow.com/a/13638998
+https://wingoodharry.wordpress.com/2015/05/18/raspberry-pi-temperature-sensor-web-server-part-3-scheduling-temperature-readings-and-php-script/
+http://wiki.hashphp.org/PDO_Tutorial_for_MySQL_Developers
+
 */
 
 // Fill these variables with the appropriate data
