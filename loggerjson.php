f1fe961f68838ea7d3378ffc28bdc36333b67c94
Mohamed <mebsim@users.noreply.github.com>
2018-07-19 13:57:56 -0400

    Update loggerjson.php

--- loggerjson.php
+++ loggerjson.php
@@ -14,21 +14,29 @@ the internet. Formats into a JSON file.
 
 */
 
+// Fill these variables with the appropriate data
 $username="";
 $password="";
 $database="";
 
+// Creates access to the database
 $pdo = new PDO('mysql:host=localhost;dbname=temp_database',$username,$password);
 
+// An empty array to fill with data
 $values = array();
 
+// A counter
 $i = 0;
 
+// Loops through each row of the database and inserts them into the empty array
 foreach($pdo->query('SELECT * FROM log') as $row) {
 	$values[$i] = $row;
 	$i++;
 }
 
+// Formats the array
 $json = json_encode($values,JSON_PRETTY_PRINT);
+
+// Prints out the database (<pre> used to maintain whitespace)
 printf("<pre>%s</pre>", $json);
 ?>
